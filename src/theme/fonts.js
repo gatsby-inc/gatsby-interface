0bdedc3c65aed09a9fdb83f0f170cf5c60065a56
greg lobinski <greglobinski@gmail.com>
2019-12-19 18:02:25 +0100

    feat: design-tokens.md

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -10,11 +10,11 @@ export default fonts
 
 /*
 
-  fonts = [
-    header: "Futura PT,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji"
-    monospace: "SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace"
-    serif: "Georgia,Times New Roman,Times,serif"
-    system: "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji"
-  ]
+  fonts = {
+    header: "Futura PT,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji",
+    monospace: "SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace",
+    serif: "Georgia,Times New Roman,Times,serif",
+    system: "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji",
+  }
 
 */
