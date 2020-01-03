3d89a78de9d7a9adc560b18d60cf63a7968da726
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-01-03 11:50:00 -0800

    chore: tiny const name change

--- src/components/Chip/BaseChip.tsx
+++ src/components/Chip/BaseChip.tsx
@@ -4,7 +4,7 @@ import { css } from "@emotion/core"
 import colors from "../../theme/colors"
 import fontSizes from "../../theme/fontSizes"
 
-const HEIGHT = `1.75rem`
+const SIZE = `1.75rem`
 const BORDER_WIDTH = `1px`
 const BACKGROUND_COLOR = colors.grey["10"]
 
@@ -12,13 +12,13 @@ const baseCss = css({
   display: `inline-flex`,
   alignItems: `center`,
   verticalAlign: `middle`,
-  minWidth: HEIGHT,
-  height: HEIGHT,
+  minWidth: SIZE,
+  height: SIZE,
   padding: `0 0.75rem`,
   backgroundColor: BACKGROUND_COLOR,
   borderRadius: `0.75rem`,
   fontSize: fontSizes[0],
-  lineHeight: `calc(${HEIGHT} - 2 * ${BORDER_WIDTH})`,
+  lineHeight: `calc(${SIZE} - 2 * ${BORDER_WIDTH})`,
   color: colors.blackFade["70"],
   border: `${BORDER_WIDTH} solid ${BACKGROUND_COLOR}`,
   whiteSpace: `nowrap`,
