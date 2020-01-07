0c4fb0400978518bff18f6d573ce1eab87dd004c
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-01-07 13:35:00 -0800

    chore: added a couple of comments

--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -2,6 +2,11 @@ import { fonts as rawFonts, FontToken } from "gatsby-design-tokens"
 
 export type Font = FontToken
 
+/**
+ * Font families are represented as an array of strings in 'gatsby-design-tokens'
+ * In order to make them more easy to use we're going to compose a string out of them
+ * that can be passed to "fontFamily" style
+ */
 const fonts: Partial<Record<Font, string>> = {} // Object.keys(baseFonts).map(key => baseFonts[key].join())
 
 for (const prop in rawFonts) {
--- src/theme/radii.ts
+++ src/theme/radii.ts
@@ -4,6 +4,16 @@ export type RadiusToken = 0 | 1 | 2 | 3 | 4 | 5 | 6
 
 export type Radii = [string, string, string, string, string, string, string]
 
+/**
+ * Radii from 'gatsby-design-tokens' are represented
+ * as numbers for pixel values and strings like '100%' for percent values
+ *
+ * While this works pretty good with Emotion for single values (e.g, "borderRadius: radii[3]"),
+ * it involves knowing implementation details when interpolating them:
+ *   borderRadius: `${radii[2]}px 0 0 ${radii[2]}px`
+ *
+ * Because of this, we're going to append 'px` to each numeric radius
+ */
 const radii = baseRadii.map(val =>
   typeof val === `number` ? `${val}px` : val
 ) as Radii
