1c515cc445392e37914ab9c2daed819cc773896c
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-02-17 14:29:09 -0800

    chore: a11y fixes in some stories

--- src/components/icons/__tests__/icons.test.js
+++ src/components/icons/__tests__/icons.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { render } from "@testing-library/react"
 
-import * as icons from "../src/components/icons/icons"
+import * as icons from "../icons"
 
 const sortedIconComponentNames = Object.keys(icons).sort()
 
