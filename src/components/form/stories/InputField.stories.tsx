c147a1371f6fe387dd8ff41ffb6979247e1d1d3e
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-03-11 10:39:18 -0700

    chore: trying out new stories structure for form fields

---
+++ src/components/form/stories/InputField.stories.tsx
@@ -0,0 +1,104 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
+
+import { DecoratorFn } from "@storybook/react"
+
+import { StoryUtils } from "../../../utils/storybook"
+import README from "../README_INPUT_FIELD.md"
+import { action } from "@storybook/addon-actions"
+import {
+  InputField,
+  InputFieldWrapper,
+  InputFieldLabel,
+  InputFieldControl,
+  InputFieldHint,
+  InputFieldError,
+} from "../components/InputField"
+import { getFieldBlockSandboxProps } from "./stories.utils"
+import { text } from "@storybook/addon-knobs"
+
+export default {
+  title: `Form — styled primitives/InputField`,
+  decorators: [
+    story => (
+      <StoryUtils.Container>
+        <StoryUtils.Stack>{story()}</StoryUtils.Stack>
+      </StoryUtils.Container>
+    ),
+  ] as DecoratorFn[],
+  parameters: {
+    readme: {
+      sidebar: README,
+    },
+  },
+}
+
+export const Basic = () => (
+  <InputField id="inputField">
+    <InputFieldWrapper>
+      <InputFieldLabel>Field label</InputFieldLabel>
+      <InputFieldControl onChange={e => action(`Change`)(e.target.value)} />
+    </InputFieldWrapper>
+  </InputField>
+)
+
+export const Sandbox = () => {
+  const {
+    label,
+    labelSize,
+    error,
+    hint,
+    disabled,
+    required,
+  } = getFieldBlockSandboxProps()
+  const placeholder = text(`Placeholder`, `This is a placeholder`)
+
+  return (
+    <InputField id="inputField" hasError={!!error} hasHint={!!hint}>
+      <InputFieldWrapper>
+        <InputFieldLabel size={labelSize} isRequired={required}>
+          {label}
+        </InputFieldLabel>
+        <InputFieldControl
+          onChange={e => action(`Change`)(e.target.value)}
+          placeholder={placeholder}
+          disabled={disabled}
+          required={required}
+        />
+        <InputFieldHint>{hint}</InputFieldHint>
+        <InputFieldError>{error}</InputFieldError>
+      </InputFieldWrapper>
+    </InputField>
+  )
+}
+
+export const WithHint = () => (
+  <InputField id="inputField" hasHint>
+    <InputFieldWrapper>
+      <InputFieldLabel>Field label</InputFieldLabel>
+      <InputFieldControl />
+      <InputFieldHint>Hint</InputFieldHint>
+    </InputFieldWrapper>
+  </InputField>
+)
+
+export const WithError = () => (
+  <InputField id="inputField" hasError>
+    <InputFieldWrapper>
+      <InputFieldLabel>Field label</InputFieldLabel>
+      <InputFieldControl />
+      <InputFieldError>Error</InputFieldError>
+    </InputFieldWrapper>
+  </InputField>
+)
+
+export const WithHintAndError = () => (
+  <InputField id="inputField" hasHint hasError>
+    <InputFieldWrapper>
+      <InputFieldLabel>Field label</InputFieldLabel>
+      <InputFieldControl />
+      <InputFieldHint>Hint</InputFieldHint>
+      <InputFieldError>Error</InputFieldError>
+    </InputFieldWrapper>
+  </InputField>
+)
