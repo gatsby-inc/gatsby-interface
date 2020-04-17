02246623b81212fa10c6eaeaec4d1685293ad16e
Florian Kissling <florian.kissling@gmail.com>
2020-04-17 14:24:08 +0200

    chore: add Figma design

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,8 +1,10 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core"
 import { text, radios, boolean } from "@storybook/addon-knobs"
-
+import { DecoratorFn } from "@storybook/react"
+import { withDesign } from "storybook-addon-designs"
 import { MdSettingsEthernet } from "react-icons/md"
+
 import { Badge } from "."
 import { BadgeVariant, BadgeTone, BadgeText, BadgeSize } from "./types"
 import {
@@ -14,6 +16,14 @@ import {
 export default {
   title: `Badge`,
   component: Badge,
+  decorators: [withDesign] as DecoratorFn[],
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/KTlXv0iPoiPZ0o2O2e1x1Aqa/Badges?node-id=57%3A51",
+    },
+  },
 }
 
 export const Basic = () => <Badge>Badge</Badge>
