9635ab17b237b287e2f473649a15ccaea1e72456
Florian Kissling <florian.kissling@gmail.com>
2020-04-17 03:39:35 +0200

    chore: fix contrast for `SUCCESS`, `WARNING`
    Analog to <Notification>.

--- src/components/Badge/Badge.helpers.tsx
+++ src/components/Badge/Badge.helpers.tsx
@@ -13,10 +13,18 @@ const variants: Record<BadgeVariant, GetVariantStylesFn> = {
     ]
   },
   PILL: tone => {
-    return theme => ({
-      background: theme.tones[tone].dark,
-      color: theme.colors.white,
-    })
+    return theme => [
+      {
+        background: theme.tones[tone].dark,
+        color: tone !== `WARNING` ? theme.colors.white : null,
+      },
+      tone === `WARNING` && {
+        background: theme.colors.yellow[50],
+      },
+      tone === `SUCCESS` && {
+        background: theme.colors.green[80],
+      },
+    ]
   },
 }
 
