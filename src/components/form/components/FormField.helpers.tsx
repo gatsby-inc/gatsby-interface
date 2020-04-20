gatsby-inc/gatsby-interface

commit 6b53a135640c37d4334c4adb62ef822203a5851c

WIP stash

src/components/form/components/FormField.helpers.tsx
@@ -38,7 +38,7 @@ export const RequiredFlag = () => (
 )
 
 export const getFocusStyles = (theme: Theme, hasError?: boolean) => ({
-  boxShadow: `0 0 0 3px ${
+  boxShadow: `0 0 0 4px ${
     hasError ? theme.colors.red[10] : theme.colors.purple[20]
   }`,
   borderColor: hasError ? theme.colors.red[30] : theme.colors.purple[60],
@@ -53,10 +53,12 @@ export const getInputStyles = (
     : `1px solid ${theme.colors.grey[30]}`,
   background: theme.colors.white,
   borderRadius: theme.radii[2],
+  // borderRadius: 3,
   color: theme.colors.grey[90],
   fontFamily: theme.fonts.system,
   fontSize: theme.fontSizes[2],
-  height: `2.25rem`,
+  // height: `2.25rem`,
+  height: theme.space[9],
   padding: `0 ${theme.space[3]}`,
   position: `relative`,
   width: `100%`,

.storybook/theming.tsx
@@ -9,12 +9,13 @@ const theme = getTheme()
 
 export const storybookThemeLight = create({
   ...themes.light,
+  appBg: theme.colors.grey[5],
   fontBase: theme.fonts.body,
   fontCode: theme.fonts.monospace,
-  colorSecondary: theme.colors.gatsby,
+  colorSecondary: theme.colors.purple[40],
   brandTitle: `Gatsby Interface`,
   barTextColor: theme.colors.grey[70],
-  barSelectedColor: theme.colors.purple[50],
+  barSelectedColor: theme.colors.purple[40],
 })
 
 export const withTheme: DecoratorFn = story => {

src/components/Button/utils/storybook-styles.tsx
@@ -151,12 +151,15 @@ export function showcaseCustomStyles<P>(
             {...defaultProps}
             onClick={action(`Button was clicked`)}
             css={theme => ({
-              color: theme.colors.purple[60],
-              background: theme.colors.yellow[60],
-              borderColor: theme.colors.yellow[60],
+              color: theme.colors.blackFade[80],
+              background: theme.colors.teal[30],
+              borderColor: theme.colors.teal[30],
+              fontWeight: theme.fontWeights.body,
 
               "&:hover:not([disabled])": {
-                color: theme.colors.white,
+                background: theme.colors.teal[50],
+                borderColor: theme.colors.teal[50],
+                color: theme.colors.black,
               },
             })}
           >

src/theme/styles/button.ts
@@ -40,10 +40,10 @@ function getButtonBaseCss(): ThemeCss {
     boxSizing: `border-box`,
     cursor: `pointer`,
     display: `inline-flex`,
-    fontFamily: theme.fonts.heading,
+    fontFamily: theme.fonts.body,
     justifyContent: `center`,
     transition: `background 0.5s, border 0.5s, color 0.5s`,
-    lineHeight: theme.lineHeights.dense,
+    lineHeight: theme.lineHeights.solid,
     textDecoration: `none`,
 
     "&[disabled], &[disabled]:hover": {
@@ -118,30 +118,31 @@ function getButtonSizeCss(size: ButtonSize): ThemeCss {
   return theme => {
     if (size === `S`) {
       return {
-        fontSize: theme.fontSizes[1],
-        minHeight: `1.6rem`,
-        padding: `0.3rem 0.5rem`,
+        // borderRadius: theme.radii[1],
+        fontSize: theme.fontSizes[0],
+        minHeight: theme.space[7],
+        padding: `0.25rem 0.5rem`,
       }
     }
     if (size === `M`) {
       return {
-        fontSize: theme.fontSizes[2],
-        minHeight: `2rem`,
-        padding: `0.45rem 0.75rem`,
+        fontSize: theme.fontSizes[1],
+        minHeight: theme.space[8],
+        padding: `0.25rem 0.75rem`,
       }
     }
     if (size === `L`) {
       return {
-        fontSize: theme.fontSizes[3],
-        minHeight: `2.4rem`,
-        padding: `0.55rem 1rem`,
+        fontSize: theme.fontSizes[2],
+        minHeight: theme.space[9],
+        padding: `0.25rem 1rem`,
       }
     }
     if (size === `XL`) {
       return {
-        fontSize: theme.fontSizes[5],
-        minHeight: `3.25rem`,
-        padding: `0.65rem 1.25rem`,
+        fontSize: theme.fontSizes[4],
+        minHeight: theme.space[10],
+        padding: `0.75rem 1.25rem`,
       }
     }
   }

commit 265c0a518faddec94b52c1d0792d8937f4ff9d5d

chore: fix input height
This reverts a bit from 6b53a13 which mistakenly changed FormField's height to 40px.
The default (and currently sole) input height should match the height of the default (`L`) button (and font-size).

We don't have that size on the scale, neither in the theme (yet).

src/components/form/components/FormField.helpers.tsx
@@ -54,12 +54,10 @@ export const getInputStyles = (
     : `1px solid ${theme.colors.grey[30]}`,
   background: theme.colors.white,
   borderRadius: theme.radii[2],
-  // borderRadius: 3,
   color: theme.colors.grey[90],
   fontFamily: theme.fonts.system,
   fontSize: theme.fontSizes[2],
-  // height: `2.25rem`,
-  height: theme.space[9],
+  height: `calc(${theme.space[2]} * 9)`,
   padding: `0 ${theme.space[3]}`,
   position: `relative`,
   width: `100%`,