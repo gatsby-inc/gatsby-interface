gatsby-inc/gatsby-interface

commit 6b53a135640c37d4334c4adb62ef822203a5851c

WIP stash

src/components/Button/utils/storybook-styles.tsx
@@ -151,12 +151,15 @@ export function showcaseCustomStyles<P>(
             {...defaultProps}
             onClick={action(`Button was clicked`)}
             css={theme => ({
-              color: theme.colors.purple[60],
-              background: theme.colors.yellow[60],
-              borderColor: theme.colors.yellow[60],
+              color: theme.colors.blackFade[80],
+              background: theme.colors.teal[30],
+              borderColor: theme.colors.teal[30],
+              fontWeight: theme.fontWeights.body,
 
               "&:hover:not([disabled])": {
-                color: theme.colors.white,
+                background: theme.colors.teal[50],
+                borderColor: theme.colors.teal[50],
+                color: theme.colors.black,
               },
             })}
           >

src/theme/styles/button.ts
@@ -40,10 +40,10 @@ function getButtonBaseCss(): ThemeCss {
     boxSizing: `border-box`,
     cursor: `pointer`,
     display: `inline-flex`,
-    fontFamily: theme.fonts.heading,
+    fontFamily: theme.fonts.body,
     justifyContent: `center`,
     transition: `background 0.5s, border 0.5s, color 0.5s`,
-    lineHeight: theme.lineHeights.dense,
+    lineHeight: theme.lineHeights.solid,
     textDecoration: `none`,
 
     "&[disabled], &[disabled]:hover": {
@@ -118,30 +118,31 @@ function getButtonSizeCss(size: ButtonSize): ThemeCss {
   return theme => {
     if (size === `S`) {
       return {
-        fontSize: theme.fontSizes[1],
-        minHeight: `1.6rem`,
-        padding: `0.3rem 0.5rem`,
+        // borderRadius: theme.radii[1],
+        fontSize: theme.fontSizes[0],
+        minHeight: theme.space[7],
+        padding: `0.25rem 0.5rem`,
       }
     }
     if (size === `M`) {
       return {
-        fontSize: theme.fontSizes[2],
-        minHeight: `2rem`,
-        padding: `0.45rem 0.75rem`,
+        fontSize: theme.fontSizes[1],
+        minHeight: theme.space[8],
+        padding: `0.25rem 0.75rem`,
       }
     }
     if (size === `L`) {
       return {
-        fontSize: theme.fontSizes[3],
-        minHeight: `2.4rem`,
-        padding: `0.55rem 1rem`,
+        fontSize: theme.fontSizes[2],
+        minHeight: theme.space[9],
+        padding: `0.25rem 1rem`,
       }
     }
     if (size === `XL`) {
       return {
-        fontSize: theme.fontSizes[5],
-        minHeight: `3.25rem`,
-        padding: `0.65rem 1.25rem`,
+        fontSize: theme.fontSizes[4],
+        minHeight: theme.space[10],
+        padding: `0.75rem 1.25rem`,
       }
     }
   }

.storybook/theming.tsx
@@ -9,12 +9,13 @@ const theme = getTheme()
 
 export const storybookThemeLight = create({
   ...themes.light,
+  appBg: theme.colors.grey[5],
   fontBase: theme.fonts.body,
   fontCode: theme.fonts.monospace,
-  colorSecondary: theme.colors.gatsby,
+  colorSecondary: theme.colors.purple[40],
   brandTitle: `Gatsby Interface`,
   barTextColor: theme.colors.grey[70],
-  barSelectedColor: theme.colors.purple[50],
+  barSelectedColor: theme.colors.purple[40],
 })
 
 export const withTheme: DecoratorFn = story => {

src/components/form/components/FormField.helpers.tsx
@@ -38,7 +38,7 @@ export const RequiredFlag = () => (
 )
 
 export const getFocusStyles = (theme: Theme, hasError?: boolean) => ({
-  boxShadow: `0 0 0 3px ${
+  boxShadow: `0 0 0 4px ${
     hasError ? theme.colors.red[10] : theme.colors.purple[20]
   }`,
   borderColor: hasError ? theme.colors.red[30] : theme.colors.purple[60],
@@ -53,10 +53,12 @@ export const getInputStyles = (
     : `1px solid ${theme.colors.grey[30]}`,
   background: theme.colors.white,
   borderRadius: theme.radii[2],
+  // borderRadius: 3,
   color: theme.colors.grey[90],
   fontFamily: theme.fonts.system,
   fontSize: theme.fontSizes[2],
-  height: `2.25rem`,
+  // height: `2.25rem`,
+  height: theme.space[9],
   padding: `0 ${theme.space[3]}`,
   position: `relative`,
   width: `100%`,

commit 252f5f2ce1d45fd81ebfb2c8b31bf8468cf04cfc

feat: make `<Button>` use `fonts.sans` by default, add prop `textVariant` to use them w/ "Futura PT" (#296)
* WIP stash

* chore: fix "L" height

* chore: try `semiBold` for `PRIMARY` button

* chore: fix input height

This reverts a bit from 6b53a13 which mistakenly changed FormField's height to 40px.
The default (and currently sole) input height should match the height of the default (`L`) button (and font-size).

We don't have that size on the scale, neither in the theme (yet).

* chore: use theme tokens

* chore: revert unnecessary noise

* chore: rm yet another stray 💩

* chore: adjust custom button styles … again

* chore: adjust button sizes once more

- `S` `~28px` -> `28px`
- `M` `~36px` -> `36px`
- `L` `~41px` -> `40px`
- `XL` `~52px` -> `48px`

* feat: add `marketing` prop

If true, `Button` will render with "Futura PT" instead of theme.fonts.body/.sans.

* feat: `marketing` -> `textVariant`

Analog to what we do for `Badge`.

* feat: fix `textVariant`, add knobs

* chore: bump snapshots

src/components/AnchorButton/AnchorButton.tsx
@@ -10,7 +10,16 @@ export const AnchorButton = React.forwardRef<
   HTMLAnchorElement,
   AnchorButtonProps
 >((props, ref) => {
-  const { children, size, tone, variant, leftIcon, rightIcon, ...rest } = props
+  const {
+    children,
+    size,
+    tone,
+    variant,
+    leftIcon,
+    rightIcon,
+    textVariant,
+    ...rest
+  } = props
 
   return (
     <BaseAnchor
@@ -21,6 +30,7 @@ export const AnchorButton = React.forwardRef<
         variant,
         leftIcon,
         rightIcon,
+        textVariant,
       })}
       {...rest}
       ref={ref}

src/components/Button/Button.tsx
@@ -9,6 +9,7 @@ import {
   ButtonVariant,
   ButtonTone,
   ButtonSize,
+  ButtonTextVariant,
 } from "../../theme/styles/button"
 import { Theme } from "../../theme"
 
@@ -18,6 +19,7 @@ export type ButtonStyleProps = {
   variant?: ButtonVariant
   leftIcon?: React.ReactNode
   rightIcon?: React.ReactNode
+  textVariant?: ButtonTextVariant
 }
 
 export type ButtonProps = BaseButtonProps & ButtonStyleProps
@@ -30,6 +32,7 @@ export function getButtonStyles({
   loading,
   leftIcon,
   rightIcon,
+  textVariant = `DEFAULT`,
 }: {
   children: React.ReactNode
   loading?: boolean
@@ -45,6 +48,7 @@ export function getButtonStyles({
       loading,
       leftIcon,
       rightIcon,
+      textVariant,
     }),
     children:
       leftIcon || rightIcon ? (
@@ -70,6 +74,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       variant,
       leftIcon,
       rightIcon,
+      textVariant,
       ...rest
     } = props
 
@@ -83,6 +88,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           variant,
           leftIcon,
           rightIcon,
+          textVariant,
         })}
         loading={loading}
         LoadingIcon={LoadingIcon}

src/components/Button/utils/storybook-styles.tsx
@@ -12,6 +12,7 @@ import {
   ButtonSize,
   ButtonTone,
   ButtonVariant,
+  ButtonTextVariant,
 } from "../../../theme/styles/button"
 import { StoryFnReactReturnType } from "@storybook/react/dist/client/preview/types"
 
@@ -31,6 +32,12 @@ const BUTTON_TONE_OPTIONS = BUTTON_TONES.reduce(enumToOptions, {})
 const BUTTON_VARIANTS: ButtonVariant[] = [`PRIMARY`, `SECONDARY`, `GHOST`]
 const BUTTON_VARIANT_OPTIONS = BUTTON_VARIANTS.reduce(enumToOptions, {})
 
+const BUTTON_TEXT_VARIANTS: ButtonTextVariant[] = [`DEFAULT`, `BRAND`]
+const BUTTON_TEXT_VARIANT_OPTIONS = BUTTON_TEXT_VARIANTS.reduce(
+  enumToOptions,
+  {}
+)
+
 type ShowcaseReturn = Parameters<StoryApi<StoryFnReactReturnType>["add"]>
 
 export function showcaseVariants<P>(
@@ -42,6 +49,11 @@ export function showcaseVariants<P>(
     () => {
       const size: ButtonSize = radios(`size`, BUTTON_SIZE_OPTIONS, `L`)
       const tone: ButtonTone = radios(`tone`, BUTTON_TONE_OPTIONS, `BRAND`)
+      const textVariant: ButtonTextVariant = radios(
+        `textVariant`,
+        BUTTON_TEXT_VARIANT_OPTIONS,
+        `DEFAULT`
+      )
 
       const renderCase = (variant: ButtonVariant) => (
         <Component
@@ -51,6 +63,7 @@ export function showcaseVariants<P>(
           tone={tone}
           size={size}
           ref={undefined}
+          textVariant={textVariant}
         >
           variant&nbsp;<React.Fragment>'{variant}'</React.Fragment>
         </Component>
@@ -78,6 +91,11 @@ export function showcaseSizes<P>(
         `PRIMARY`
       )
       const tone: ButtonTone = radios(`tone`, BUTTON_TONE_OPTIONS, `BRAND`)
+      const textVariant: ButtonTextVariant = radios(
+        `textVariant`,
+        BUTTON_TEXT_VARIANT_OPTIONS,
+        `DEFAULT`
+      )
 
       const renderCase = (size: ButtonSize) => (
         <Component
@@ -87,6 +105,7 @@ export function showcaseSizes<P>(
           tone={tone}
           size={size}
           ref={undefined}
+          textVariant={textVariant}
         >
           size&nbsp;<React.Fragment>'{size}'</React.Fragment>
         </Component>
@@ -114,6 +133,11 @@ export function showcaseTones<P>(
         `PRIMARY`
       )
       const size: ButtonSize = radios(`size`, BUTTON_SIZE_OPTIONS, `L`)
+      const textVariant: ButtonTextVariant = radios(
+        `textVariant`,
+        BUTTON_TEXT_VARIANT_OPTIONS,
+        `DEFAULT`
+      )
 
       const renderCase = (tone: ButtonTone) => (
         <Component
@@ -123,6 +147,7 @@ export function showcaseTones<P>(
           tone={tone}
           size={size}
           ref={undefined}
+          textVariant={textVariant}
         >
           tone&nbsp;<React.Fragment>'{tone}'</React.Fragment>
         </Component>
@@ -151,12 +176,14 @@ export function showcaseCustomStyles<P>(
             {...defaultProps}
             onClick={action(`Button was clicked`)}
             css={theme => ({
-              color: theme.colors.purple[60],
-              background: theme.colors.yellow[60],
-              borderColor: theme.colors.yellow[60],
+              color: theme.colors.blackFade[80],
+              background: theme.colors.teal[20],
+              borderColor: theme.colors.teal[20],
 
               "&:hover:not([disabled])": {
-                color: theme.colors.white,
+                background: theme.colors.teal[30],
+                borderColor: theme.colors.teal[30],
+                color: theme.colors.black,
               },
             })}
           >

src/theme/styles/button.ts
@@ -4,6 +4,7 @@ import { Theme, ThemeCss } from ".."
 export type ButtonSize = "XL" | "L" | "M" | "S"
 export type ButtonTone = "BRAND" | "SUCCESS" | "DANGER" | "NEUTRAL"
 export type ButtonVariant = "PRIMARY" | "SECONDARY" | "GHOST"
+export type ButtonTextVariant = "DEFAULT" | "BRAND"
 
 export function getButtonCss({
   size = `L`,
@@ -12,6 +13,7 @@ export function getButtonCss({
   leftIcon,
   rightIcon,
   loading,
+  textVariant = `DEFAULT`,
 }: {
   size?: ButtonSize
   tone?: ButtonTone
@@ -19,20 +21,21 @@ export function getButtonCss({
   leftIcon?: React.ReactNode
   rightIcon?: React.ReactNode
   loading?: boolean
+  textVariant?: ButtonTextVariant
 }): InterpolationWithTheme<Theme> {
   return (theme: Theme) => [
-    getButtonBaseCss()(theme),
+    getButtonBaseCss(textVariant)(theme),
     getButtonIconsCss({
       hasLeftIcon: !!leftIcon,
       hasRightIcon: !!rightIcon || loading,
     })(theme),
     getButtonLoadingCss({ loading })(theme),
     getButtonVariantCss(variant, tone)(theme),
-    getButtonSizeCss(size)(theme),
+    getButtonSizeCss(size, textVariant)(theme),
   ]
 }
 
-function getButtonBaseCss(): ThemeCss {
+function getButtonBaseCss(textVariant?: ButtonTextVariant): ThemeCss {
   return theme => ({
     alignItems: `center`,
     border: theme.colors.grey[60],
@@ -40,10 +43,11 @@ function getButtonBaseCss(): ThemeCss {
     boxSizing: `border-box`,
     cursor: `pointer`,
     display: `inline-flex`,
-    fontFamily: theme.fonts.heading,
+    fontFamily:
+      textVariant === "BRAND" ? theme.fonts.heading : theme.fonts.body,
     justifyContent: `center`,
     transition: `background 0.5s, border 0.5s, color 0.5s`,
-    lineHeight: theme.lineHeights.dense,
+    lineHeight: theme.lineHeights.solid,
     textDecoration: `none`,
 
     "&[disabled], &[disabled]:hover": {
@@ -114,34 +118,41 @@ function getButtonLoadingCss({ loading }: { loading?: boolean }): ThemeCss {
       : {}
 }
 
-function getButtonSizeCss(size: ButtonSize): ThemeCss {
+function getButtonSizeCss(
+  size: ButtonSize,
+  textVariant?: ButtonTextVariant
+): ThemeCss {
   return theme => {
     if (size === `S`) {
       return {
-        fontSize: theme.fontSizes[1],
-        minHeight: `1.6rem`,
-        padding: `0.3rem 0.5rem`,
+        fontSize:
+          textVariant === "BRAND" ? theme.fontSizes[1] : theme.fontSizes[0],
+        minHeight: `calc(${theme.space[2]} * 7)`,
+        padding: `${theme.space[2]} ${theme.space[3]}`,
       }
     }
     if (size === `M`) {
       return {
-        fontSize: theme.fontSizes[2],
-        minHeight: `2rem`,
-        padding: `0.45rem 0.75rem`,
+        fontSize:
+          textVariant === "BRAND" ? theme.fontSizes[2] : theme.fontSizes[1],
+        minHeight: `calc(${theme.space[2]} * 9)`,
+        padding: `${theme.space[2]} ${theme.space[4]}`,
       }
     }
     if (size === `L`) {
       return {
-        fontSize: theme.fontSizes[3],
-        minHeight: `2.4rem`,
-        padding: `0.55rem 1rem`,
+        fontSize:
+          textVariant === "BRAND" ? theme.fontSizes[3] : theme.fontSizes[2],
+        minHeight: theme.space[9],
+        padding: `${theme.space[2]} ${theme.space[5]}`,
       }
     }
     if (size === `XL`) {
       return {
-        fontSize: theme.fontSizes[5],
-        minHeight: `3.25rem`,
-        padding: `0.65rem 1.25rem`,
+        fontSize:
+          textVariant === "BRAND" ? theme.fontSizes[5] : theme.fontSizes[4],
+        minHeight: theme.space[10],
+        padding: `${theme.space[3]} ${theme.space[6]}`,
       }
     }
   }
@@ -157,7 +168,7 @@ function getButtonVariantCss(
         background: theme.tones[tone].dark,
         border: `1px solid ${theme.tones[tone].dark}`,
         color: theme.colors.white,
-        fontWeight: `bold`,
+        fontWeight: theme.fontWeights.semiBold,
         ":hover": {
           background: theme.tones[tone].darker,
           border: `1px solid ${theme.tones[tone].darker}`,

__tests__/__snapshots__/Button.js.snap
@@ -3,7 +3,7 @@
 exports[`<PrimaryButton> renders unchanged 1`] = `
 <div>
   <button
-    class="css-9qeu11"
+    class="css-e6cl70"
     type="button"
   >
     Click me!

src/components/CopyButton/__tests__/__snapshots__/CopyButton.test.tsx.snap
@@ -3,7 +3,7 @@
 exports[`<CopyButton> renders unchanged 1`] = `
 <div>
   <button
-    class="css-1wkaa75-baseCss"
+    class="css-1spz73u-baseCss"
     title="Copy to clipboard"
     type="button"
   >