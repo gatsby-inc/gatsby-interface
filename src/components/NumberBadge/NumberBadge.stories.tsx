c2f0282ab514397fc25be815c607268dbfe5f38e
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-05-01 14:33:06 -0700

    feat: add tones support for NumberBadge

--- src/components/NumberBadge/NumberBadge.stories.tsx
+++ src/components/NumberBadge/NumberBadge.stories.tsx
@@ -1,7 +1,12 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core"
-import { number } from "@storybook/addon-knobs"
-import { NumberBadge } from "."
+import { radios, number } from "@storybook/addon-knobs"
+import {
+  radioKnobOptions,
+  withVariationsContainer,
+} from "../../utils/storybook"
+import { NumberBadge, NumberBadgeTone } from "."
+import { StoryPropVariant } from "../../utils/storybook/components"
 
 export default {
   title: `NumberBadge`,
@@ -10,10 +15,31 @@ export default {
 
 export const Basic = () => <NumberBadge>8</NumberBadge>
 
-export const Sandbox = () => <NumberBadge>{number("content", 8)}</NumberBadge>
+const TONES: NumberBadgeTone[] = [`NEUTRAL`, `WARNING`, `DANGER`]
+
+export const Sandbox = () => (
+  <NumberBadge
+    tone={radios(`tone`, radioKnobOptions<NumberBadgeTone>(TONES), `NEUTRAL`)}
+  >
+    {number("content", 8)}
+  </NumberBadge>
+)
 
 Sandbox.story = {
   parameters: {
     chromatic: { disable: true },
   },
+}
+
+export const Tones = () =>
+  TONES.map(tone => (
+    <StoryPropVariant propName="tone" propValue={tone}>
+      <NumberBadge key={tone} tone={tone}>
+        8
+      </NumberBadge>
+    </StoryPropVariant>
+  ))
+
+Tones.story = {
+  decorators: [withVariationsContainer],
 }
