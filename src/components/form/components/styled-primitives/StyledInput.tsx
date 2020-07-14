d771346c9ecff6858b7f71fec67569f98aec4327
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-07-14 16:51:44 -0700

    feat(forms): support left icons in StyledInput

--- src/components/form/components/styled-primitives/StyledInput.tsx
+++ src/components/form/components/styled-primitives/StyledInput.tsx
@@ -4,18 +4,42 @@ import React from "react"
 import { ThemeCss } from "../../../../theme"
 import { baseInputCss } from "../../styles"
 
-export type StyledInputProps = React.ComponentPropsWithRef<"input">
-
-export const StyledInput = React.forwardRef<
-  HTMLInputElement,
-  React.ComponentPropsWithRef<"input">
->(function InputFieldControl(props, ref) {
-  const placeholder =
-    props.placeholder && props.disabled
-      ? `The field is disabled`
-      : props.placeholder
-
-  const baseCss: ThemeCss = theme => [baseInputCss(theme), { width: `auto` }]
-
-  return <input ref={ref} css={baseCss} {...props} placeholder={placeholder} />
+const leftIconCss: ThemeCss = theme => ({
+  position: `absolute`,
+  color: theme.colors.grey[60],
+  fontSize: theme.fontSizes[3],
+  top: `calc((100% - ${theme.fontSizes[3]}) / 2)`,
+  margin: `0 ${theme.space[3]}`,
+  zIndex: 2,
 })
+
+const inputWithLeftIconCss: ThemeCss = theme => ({
+  // offset padding based on left icon spacing and size
+  paddingLeft: `calc(${theme.fontSizes[3]} + 2 * ${theme.space[3]})`,
+})
+
+export type StyledInputProps = React.ComponentPropsWithRef<"input"> & {
+  LeftIcon?: React.ComponentType<any>
+}
+
+export const StyledInput = React.forwardRef<HTMLInputElement, StyledInputProps>(
+  function InputFieldControl({ LeftIcon, className, ...props }, ref) {
+    const placeholder =
+      props.placeholder && props.disabled
+        ? `The field is disabled`
+        : props.placeholder
+
+    const baseCss: ThemeCss = theme => [
+      baseInputCss(theme),
+      { width: `auto` },
+      LeftIcon && inputWithLeftIconCss(theme),
+    ]
+
+    return (
+      <div className={className} css={{ position: `relative` }}>
+        {LeftIcon && <LeftIcon css={leftIconCss} size="inherit" aria-hidden />}
+        <input ref={ref} css={baseCss} {...props} placeholder={placeholder} />
+      </div>
+    )
+  }
+)
