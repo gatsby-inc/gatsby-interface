93a8cf22859ce192123d288159b98d134015f384
Aleksei <elanhant@gmail.com>
2020-08-05 12:43:52 -0700

    feat: SplitButton component (#371)

---
+++ src/components/SplitButton/SplitButton.stories.tsx
@@ -0,0 +1,194 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
+import React from "react"
+import { text, radios, boolean } from "@storybook/addon-knobs"
+import {
+  radioKnobOptions,
+  withVariationsContainer,
+  getStoryOptions,
+  disableAnimationsDecorator,
+} from "../../utils/storybook"
+import { SplitButton, SplitButtonVariant } from "."
+import { ButtonTone, ButtonSize, ButtonWidth } from "../../theme/styles/button"
+import { DropdownMenuItem } from "../DropdownMenu"
+import { DecoratorFn } from "@storybook/react"
+import isChromatic from "storybook-chromatic/isChromatic"
+
+export default {
+  title: `SplitButton`,
+  component: SplitButton,
+  subcomponents: { DropdownMenuItem },
+  decorators: [disableAnimationsDecorator],
+}
+
+const options = getStoryOptions("short")
+
+const withOpenMenuOnMount = (selector = "button"): DecoratorFn => {
+  return story => {
+    React.useEffect(() => {
+      const button = document.querySelector<HTMLButtonElement>(selector)
+      if (button) {
+        // Toggle menu for Chromatic snapshots
+        button.focus()
+        button.dispatchEvent(
+          new KeyboardEvent("keydown", {
+            bubbles: true,
+            cancelable: true,
+            key: "Enter",
+            shiftKey: false,
+          })
+        )
+      }
+    }, [])
+
+    return <div css={isChromatic() && { height: `100vh` }}>{story()}</div>
+  }
+}
+
+export const Basic = () => (
+  <SplitButton buttonLabel="Button" dropdownButtonLabel="Other options">
+    {options.map(({ value, label }) => (
+      <DropdownMenuItem onSelect={() => console.log(value)}>
+        {label}
+      </DropdownMenuItem>
+    ))}
+  </SplitButton>
+)
+
+Basic.story = {
+  decorators: [withOpenMenuOnMount(`button[aria-haspopup="true"]`)],
+}
+
+const VARIANTS: SplitButtonVariant[] = [`PRIMARY`, `SECONDARY`]
+
+export const Sandbox = () => (
+  <SplitButton
+    buttonLabel={text("buttonLabel", "Button")}
+    dropdownButtonLabel={text("dropdownButtonLabel", "Other options")}
+    disabled={boolean("disabled", false)}
+    variant={radios(
+      `variant`,
+      radioKnobOptions<SplitButtonVariant>(VARIANTS),
+      `PRIMARY`
+    )}
+    tone={radios(`tone`, radioKnobOptions<ButtonTone>(TONES), `BRAND`)}
+    size={radios(`size`, radioKnobOptions<ButtonSize>(SIZES), `L`)}
+    width={radios(`width`, radioKnobOptions<ButtonWidth>(WIDTHS), `AUTO`)}
+  >
+    {options.map(({ value, label }) => (
+      <DropdownMenuItem onSelect={() => console.log(value)}>
+        {label}
+      </DropdownMenuItem>
+    ))}
+  </SplitButton>
+)
+
+Sandbox.story = {
+  parameters: {
+    chromatic: { disable: true },
+  },
+}
+
+export const Variants = () =>
+  VARIANTS.map(variant => (
+    <SplitButton
+      key={variant}
+      variant={variant}
+      buttonLabel={`Variant: ${variant}`}
+      dropdownButtonLabel="Other options"
+    >
+      {options.map(({ value, label }) => (
+        <DropdownMenuItem onSelect={() => console.log(value)}>
+          {label}
+        </DropdownMenuItem>
+      ))}
+    </SplitButton>
+  ))
+
+Variants.story = {
+  decorators: [withVariationsContainer],
+}
+
+const TONES: ButtonTone[] = [`BRAND`, `NEUTRAL`, `SUCCESS`, `DANGER`]
+
+export const Tones = () =>
+  TONES.map(tone => (
+    <SplitButton
+      key={tone}
+      tone={tone}
+      buttonLabel={`Tone: ${tone}`}
+      dropdownButtonLabel="Other options"
+    >
+      {options.map(({ value, label }) => (
+        <DropdownMenuItem onSelect={() => console.log(value)}>
+          {label}
+        </DropdownMenuItem>
+      ))}
+    </SplitButton>
+  ))
+
+Tones.story = {
+  decorators: [withVariationsContainer],
+}
+
+const SIZES: ButtonSize[] = [`S`, `M`, `L`, `XL`]
+
+export const Sizes = () =>
+  SIZES.map(size => (
+    <SplitButton
+      key={size}
+      size={size}
+      buttonLabel={`Size: ${size}`}
+      dropdownButtonLabel="Other options"
+    >
+      {options.map(({ value, label }) => (
+        <DropdownMenuItem onSelect={() => console.log(value)}>
+          {label}
+        </DropdownMenuItem>
+      ))}
+    </SplitButton>
+  ))
+
+Sizes.story = {
+  decorators: [withVariationsContainer],
+}
+
+const WIDTHS: ButtonWidth[] = [`AUTO`, `FIT_CONTAINER`]
+
+export const Widths = () =>
+  WIDTHS.map(width => (
+    <SplitButton
+      key={width}
+      width={width}
+      buttonLabel={`Width: ${width}`}
+      dropdownButtonLabel="Other options"
+      data-testid={width}
+    >
+      {options.map(({ value, label }) => (
+        <DropdownMenuItem onSelect={() => console.log(value)}>
+          {label}
+        </DropdownMenuItem>
+      ))}
+    </SplitButton>
+  ))
+
+Widths.story = {
+  decorators: [
+    withVariationsContainer,
+    withOpenMenuOnMount(`button[data-testid="FIT_CONTAINER"] ~ button`),
+  ],
+}
+
+export const Disabled = () => (
+  <SplitButton
+    buttonLabel="Button"
+    dropdownButtonLabel="Other options"
+    disabled
+  >
+    {options.map(({ value, label }) => (
+      <DropdownMenuItem onSelect={() => console.log(value)}>
+        {label}
+      </DropdownMenuItem>
+    ))}
+  </SplitButton>
+)
---
+++ src/components/SplitButton/SplitButton.tsx
@@ -0,0 +1,130 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
+import React from "react"
+import { ThemeCss } from "../../theme"
+import { ButtonVariant } from "../../theme/styles/button"
+import {
+  DropdownMenu,
+  DropdownMenuButton,
+  DropdownMenuItemsLowLevel,
+  DropdownMenuPopover,
+} from "../DropdownMenu"
+import { Position, positionRight } from "@reach/popover"
+import { useIconButton } from "../IconButton"
+import { MdKeyboardArrowDown } from "react-icons/md"
+import { Button, ButtonProps } from "../Button"
+
+const baseCss: ThemeCss = _theme => ({
+  display: `inline-flex`,
+  alignItems: `center`,
+})
+
+const primaryButtonCss: ThemeCss = _theme => ({
+  borderTopRightRadius: 0,
+  borderBottomRightRadius: 0,
+  borderRightColor: `transparent`,
+})
+
+const separatorCss: ThemeCss = _theme => ({
+  display: `inline-block`,
+  width: 1,
+  alignSelf: `stretch`,
+})
+
+export type SplitButtonVariant = Exclude<ButtonVariant, "GHOST">
+
+export type SplitButtonProps = Omit<
+  ButtonProps,
+  | "variant"
+  | "leftIcon"
+  | "rightIcon"
+  | "loading"
+  | "loadingLabel"
+  | "LoadingIcon"
+> & {
+  buttonLabel: React.ReactNode
+  dropdownButtonLabel: React.ReactNode
+  children?: React.ReactNode
+  variant?: SplitButtonVariant
+}
+
+export function SplitButton({
+  buttonLabel,
+  dropdownButtonLabel,
+  children,
+  variant,
+  size,
+  tone = `BRAND`,
+  disabled,
+  ...rest
+}: SplitButtonProps) {
+  const containerRef = React.useRef<HTMLDivElement>(null)
+
+  const iconButtonProps = useIconButton({
+    icon: <MdKeyboardArrowDown />,
+    children: dropdownButtonLabel,
+    variant,
+    size,
+    tone,
+  })
+
+  const finalIconButtonCss: ThemeCss = theme => [
+    iconButtonProps.css(theme),
+    {
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+      flexShrink: 0,
+    },
+  ]
+
+  const finalSeparatorCss: ThemeCss = theme => [
+    separatorCss(theme),
+    variant === `PRIMARY` && {
+      background: `
+        linear-gradient(${theme.colors.whiteFade[30]} 0%, ${theme.colors.whiteFade[30]} 100%),
+        linear-gradient(${theme.tones[tone].dark} 0%, ${theme.tones[tone].dark} 100%)`,
+    },
+    variant === `SECONDARY` && {
+      backgroundColor: `transparent`,
+      borderTop: `1px solid ${theme.tones[tone].light}`,
+      borderBottom: `1px solid ${theme.tones[tone].light}`,
+    },
+  ]
+
+  const positionMenuPopover: Position = (targetRect, popoverRect) => {
+    return {
+      ...positionRight(targetRect, popoverRect),
+      minWidth: containerRef.current
+        ? containerRef.current.clientWidth
+        : `auto`,
+    }
+  }
+
+  return (
+    <div ref={containerRef} css={baseCss}>
+      <Button
+        variant={variant}
+        size={size}
+        tone={tone}
+        disabled={disabled}
+        css={primaryButtonCss}
+        {...rest}
+      >
+        {buttonLabel}
+      </Button>
+      <span css={finalSeparatorCss} aria-hidden />
+      <DropdownMenu>
+        <DropdownMenuButton
+          {...iconButtonProps}
+          css={finalIconButtonCss}
+          disabled={disabled}
+        />
+        <DropdownMenuPopover position={positionMenuPopover}>
+          <DropdownMenuItemsLowLevel css={{ width: `auto` }}>
+            {children}
+          </DropdownMenuItemsLowLevel>
+        </DropdownMenuPopover>
+      </DropdownMenu>
+    </div>
+  )
+}
