421cfb59549851b6183434fc3173325fcdb35737
Julien Poissonnier <julien@caffeine.lu>
2020-10-02 11:19:21 +0200

    feat(BuildLogs): Display docsUrl and errorUrl for errors (#395)

--- src/components/BuildLogs/FormattedLogMessage.tsx
+++ src/components/BuildLogs/FormattedLogMessage.tsx
@@ -3,10 +3,12 @@ import { jsx } from "@emotion/core"
 import { BuildLogItem, StructuredLogLevel } from "./types"
 import { ThemeCss, Theme } from "../../theme"
 import { FormattedMessage } from "./FormattedMessage"
+import { Link } from "../Link"
+import { Text } from "../Text"
 
 export type FormattedLogMessageProps = Pick<
   BuildLogItem,
-  "level" | "message"
+  "level" | "message" | "docsUrl" | "location" | "filePath" | "errorUrl"
 > & {
   className?: string
 }
@@ -14,11 +16,28 @@ export type FormattedLogMessageProps = Pick<
 export function FormattedLogMessage({
   level,
   message,
+  docsUrl,
+  location,
+  filePath,
+  errorUrl,
   className,
 }: FormattedLogMessageProps) {
   return (
     <div css={getMessageCss(level)} className={className}>
       <FormattedMessage rawMessage={message || ""} />
+      {docsUrl && (
+        <Text>
+          For more details see <Link href={docsUrl}>{docsUrl}</Link>
+        </Text>
+      )}
+      {location && location.start && errorUrl && (
+        <Text>
+          The error occured in{" "}
+          <Link href={errorUrl}>
+            {filePath}@{location.start.line},{location.start.column}
+          </Link>
+        </Text>
+      )}
     </div>
   )
 }
@@ -34,6 +53,25 @@ function getMessageCss(level: StructuredLogLevel | null | undefined): ThemeCss {
     }
 
     return {
+      div: {
+        // The first div > p is the title of the message.
+        p: {
+          "&:first-of-type": {
+            color: getLogLevelColor(level)(theme),
+            fontSize: theme.fontSizes[1],
+            marginBottom: theme.space[3],
+          },
+        },
+        // If we only have a single p in a single div, we don't want the margin.
+        "&:only-child": {
+          p: {
+            "&:only-child": {
+              marginBottom: 0,
+            },
+          },
+        },
+      },
+
       whiteSpace: `pre-wrap`,
       display: `block`,
 
@@ -82,15 +120,11 @@ function getMessageCss(level: StructuredLogLevel | null | undefined): ThemeCss {
 
       p: [
         spaceMixin,
+
         {
           color: theme.colors.grey[50],
           fontSize: theme.fontSizes[0],
           lineHeight: theme.lineHeights.loose,
-
-          "&:first-of-type": {
-            color: getLogLevelColor(level)(theme),
-            fontSize: theme.fontSizes[1],
-          },
 
           a: {
             textDecoration: `none`,
