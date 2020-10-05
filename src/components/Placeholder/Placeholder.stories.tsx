d48a87852f039f10ee9b265263b17bfd9a385594
mfrachet <marvin@gatsbyjs.com>
2020-10-05 14:12:27 +0200

    Moving to grid

--- src/components/Placeholder/Placeholder.stories.tsx
+++ src/components/Placeholder/Placeholder.stories.tsx
@@ -2,7 +2,6 @@
 import { jsx } from "@emotion/core"
 import { Placeholder, PlaceholderBox } from "./"
 import { fadeAnimationCss } from "./Fade"
-import { Spacer } from "../Spacer"
 import { disableAnimationsDecorator } from "../../utils/storybook/disableAnimationsDecorator"
 import { DecoratorFn } from "@storybook/react"
 
@@ -21,74 +20,26 @@ export const PlaceholderDefault = () => {
     <div css={{ width: "400px" }}>
       <Placeholder
         animation={fadeAnimationCss}
-        Left={() => <PlaceholderBox height="40px" width="40px" />}
-        Right={() => <PlaceholderBox height="40px" width="40px" />}
+        css={{
+          gridTemplateColumns: "auto 1fr auto",
+          gridTemplateRows: "1fr 1fr 1fr",
+          gridTemplateAreas: `
+          "left top right"
+          "left middle right"
+          "left bottom right"`,
+        }}
       >
-        <PlaceholderBox />
-        <PlaceholderBox width="30%" />
-        <PlaceholderBox width="80%" noSpace />
-      </Placeholder>
-    </div>
-  )
-}
+        <PlaceholderBox width="40px" height="40px" css={{ gridArea: "left" }} />
 
-export const PlaceholderOnlyRight = () => {
-  return (
-    <div css={{ width: "400px" }}>
-      <Placeholder Right={() => <PlaceholderBox height="40px" width="40px" />}>
-        <PlaceholderBox />
-        <PlaceholderBox width="40%" />
-        <PlaceholderBox width="30%" noSpace />
-      </Placeholder>
-    </div>
-  )
-}
+        <PlaceholderBox width="70%" css={{ gridArea: "top" }} />
+        <PlaceholderBox css={{ gridArea: "middle" }} />
+        <PlaceholderBox width="30%" css={{ gridArea: "bottom" }} />
 
-export const PlaceholderOnlyLeft = () => {
-  return (
-    <div css={{ width: "400px" }}>
-      <Placeholder
-        Left={() => <PlaceholderBox height="40px" width="40px" radii="50%" />}
-      >
-        <PlaceholderBox />
-
-        <PlaceholderBox width="70%" />
-        <PlaceholderBox width="20%" noSpace />
-      </Placeholder>
-    </div>
-  )
-}
-
-export const PlaceholderComplexPage = () => {
-  return (
-    <div css={{ width: "800px", position: "relative" }}>
-      <Placeholder animation={fadeAnimationCss}>
-        <PlaceholderBox width="50%" noSpace />
-        <Spacer size={5} />
-
-        <PlaceholderBox width="70%" />
-
-        <Spacer size={5} />
-
-        <div css={{ display: "flex", flexDirection: "row" }}>
-          <PlaceholderBox width="100px" />
-          <Spacer size={5} direction="horizontal" />
-          <PlaceholderBox width="100px" />
-        </div>
-
-        <Spacer size={12} />
-
-        <div css={{ display: "flex", flexDirection: "row" }}>
-          <PlaceholderBox width="50px" />
-          <Spacer size={5} direction="horizontal" />
-          <PlaceholderBox width="50px" />
-        </div>
-
-        <Spacer size={7} />
-
-        <PlaceholderBox height="150px" noSpace />
-        <Spacer size={5} />
-        <PlaceholderBox height="150px" noSpace />
+        <PlaceholderBox
+          width="40px"
+          height="40px"
+          css={{ gridArea: "right" }}
+        />
       </Placeholder>
     </div>
   )
--- src/components/Placeholder/Placeholder.tsx
+++ src/components/Placeholder/Placeholder.tsx
@@ -2,53 +2,50 @@
 import { jsx } from "@emotion/core"
 import React, { useContext } from "react"
 import { ThemeCss, Theme } from "../../theme"
-import { Radii } from "../../theme/radii"
 import { SpacerSize } from "../Spacer"
 
+const mergeCss = (sharedCss: any, css: any, theme: Theme) => {
+  if (css) {
+    if (typeof css === "function") {
+      return { ...css(theme), ...sharedCss }
+    }
+
+    return { ...css(theme), ...sharedCss }
+  }
+
+  return sharedCss
+}
+
 const PlaceholderContext = React.createContext<{
-  space: SpacerSize
   animation: string
 }>({
-  space: 0,
   animation: "",
 })
 
 export interface PlaceholderProps
   extends React.ComponentPropsWithoutRef<"div"> {
-  Left?: React.ComponentType
-  Right?: React.ComponentType
   space?: SpacerSize
   animation?: string
+  css?: any
 }
 
 export const Placeholder: React.FC<PlaceholderProps> = ({
   children,
-  Left,
-  Right,
-  space = 0,
+  space = 3,
   animation = "",
+  css,
   ...props
 }) => {
+  const computeCss: ThemeCss = theme => {
+    const sharedCss = { display: "grid", gap: theme.space[space] }
+
+    return mergeCss(sharedCss, css, theme)
+  }
+
   return (
-    <PlaceholderContext.Provider value={{ space, animation }}>
-      <div
-        css={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-        {...props}
-      >
-        {Left && (
-          <div css={{ marginRight: space, display: "block" }}>
-            <Left />
-          </div>
-        )}
-        <div css={{ flex: 1 }}>{children}</div>
-        {Right && (
-          <div css={{ marginLeft: space, display: "block" }}>
-            <Right />
-          </div>
-        )}
+    <PlaceholderContext.Provider value={{ animation }}>
+      <div css={computeCss} {...props}>
+        {children}
       </div>
     </PlaceholderContext.Provider>
   )
@@ -64,32 +61,34 @@ export interface PlaceholderBoxProps
   width?: string
   height?: string
   noSpace?: boolean
-  radii?: Radii
+  radii?: number
+  css?: any
 }
 
 export const PlaceholderBox: React.FC<PlaceholderBoxProps> = ({
   height = "14px",
   width = "100%",
-  noSpace,
-  radii,
+  radii = 2,
+  css,
   ...props
 }) => {
-  const { space, animation } = useContext(PlaceholderContext)
+  const { animation } = useContext(PlaceholderContext)
 
-  const marginBottom = noSpace ? 0 : space
+  const computeCss: ThemeCss = theme => {
+    const sharedCss = {
+      width,
+      height,
+
+      borderRadius: theme.radii[radii],
+      animation,
+    }
+
+    return mergeCss(sharedCss, css, theme)
+  }
 
   return (
     <div
-      css={(theme: Theme) => [
-        placeholderLineCss(theme),
-        {
-          width,
-          height,
-          marginBottom,
-          borderRadius: radii || theme.radii[2],
-          animation,
-        },
-      ]}
+      css={(theme: Theme) => [placeholderLineCss(theme), computeCss(theme)]}
       {...props}
     />
   )
