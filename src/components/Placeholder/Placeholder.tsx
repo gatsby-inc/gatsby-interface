659074a76bd8d8b97f0450f2a58a15951e007ebf
mfrachet <marvin@gatsbyjs.com>
2020-10-06 10:39:33 +0200

    Use className instead of css

--- src/components/Placeholder/Placeholder.tsx
+++ src/components/Placeholder/Placeholder.tsx
@@ -4,18 +4,6 @@ import React, { useContext } from "react"
 import { ThemeCss, Theme } from "../../theme"
 import { SpacerSize } from "../Spacer"
 
-const mergeCss = (sharedCss: any, css: any, theme: Theme) => {
-  if (css) {
-    if (typeof css === "function") {
-      return { ...css(theme), ...sharedCss }
-    }
-
-    return { ...css(theme), ...sharedCss }
-  }
-
-  return sharedCss
-}
-
 const PlaceholderContext = React.createContext<{
   animation: string
 }>({
@@ -33,18 +21,14 @@ export const Placeholder: React.FC<PlaceholderProps> = ({
   children,
   space = 3,
   animation = "",
-  css,
   ...props
 }) => {
-  const computeCss: ThemeCss = theme => {
-    const sharedCss = { display: "grid", gap: theme.space[space] }
-
-    return mergeCss(sharedCss, css, theme)
-  }
-
   return (
     <PlaceholderContext.Provider value={{ animation }}>
-      <div css={computeCss} {...props}>
+      <div
+        css={theme => ({ display: "grid", gap: theme.space[space] })}
+        {...props}
+      >
         {children}
       </div>
     </PlaceholderContext.Provider>
@@ -69,26 +53,22 @@ export const PlaceholderBox: React.FC<PlaceholderBoxProps> = ({
   height = "14px",
   width = "100%",
   radii = 2,
-  css,
   ...props
 }) => {
   const { animation } = useContext(PlaceholderContext)
 
-  const computeCss: ThemeCss = theme => {
-    const sharedCss = {
-      width,
-      height,
-
-      borderRadius: theme.radii[radii],
-      animation,
-    }
-
-    return mergeCss(sharedCss, css, theme)
-  }
-
   return (
     <div
-      css={(theme: Theme) => [placeholderLineCss(theme), computeCss(theme)]}
+      css={(theme: Theme) => [
+        placeholderLineCss(theme),
+        {
+          width,
+          height,
+
+          borderRadius: theme.radii[radii],
+          animation,
+        },
+      ]}
       {...props}
     />
   )
