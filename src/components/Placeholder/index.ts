124fcf034b6584812ab5128ddcd96fc6177094bd
Marvin Frachet <marvin@gatsbyjs.com>
2020-10-07 09:09:38 +0200

    Update src/components/Placeholder/index.ts

--- src/components/Placeholder/index.ts
+++ src/components/Placeholder/index.ts
@@ -1,9 +1,1 @@
-import {
-  PlaceholderProps as PlaceholderPropsDefinition,
-  PlaceholderBoxProps as PlaceholderBoxPropsDefinition,
-} from "./Placeholder"
-
-export type PlaceholderProps = PlaceholderPropsDefinition
-export type PlaceholderBoxProps = PlaceholderBoxPropsDefinition
-
-export { Placeholder, PlaceholderBox } from "./Placeholder"
+export * from "./Placeholder"
