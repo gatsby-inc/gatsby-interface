1085eebf3ac938b6b17ab5a04d52df6edd3c375f
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-10-19 08:39:00 -0700

    fix(icons): prevent plan icons from having weird black borders

--- src/components/icons/TeamPlanIcon.tsx
+++ src/components/icons/TeamPlanIcon.tsx
@@ -7,7 +7,7 @@ export default function TeamPlanIcon(props: IconProps) {
   const theme = useTheme()
 
   return (
-    <IconSkeleton {...props} iconName="TeamPlanIcon">
+    <IconSkeleton {...props} iconName="TeamPlanIcon" stroke="none">
       <path
         d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
         stroke={theme.colors.magenta[20]}
