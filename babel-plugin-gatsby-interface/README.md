cb0295d536a37e1880f0efa1d71e8675261cd487
Ward Peeters <ward@coding-tech.com>
2020-11-13 14:58:05 +0100

    feat(babel-plugin-gatsby-interface): add better esm support for gatsby

---
+++ babel-plugin-gatsby-interface/README.md
@@ -0,0 +1,5 @@
+# babel-plugin-gatsby-interface
+
+A simple transform to cherry-pick gatsby-interface modules so you don’t have to.
+
+It's necessary to optimize treeshaking in webpack
---
+++ babel-plugin-gatsby-interface/__tests__/fixtures/general/components/output.mjs
@@ -0,0 +1,8 @@
+import { Heading as _Heading } from "gatsby-interface/dist/components/Heading/Heading.esm.js";
+import { Avatar as _Avatar } from "gatsby-interface/dist/components/Avatar/Avatar.esm.js";
+import { LinkButton as _LinkButton } from "gatsby-interface/dist/components/LinkButton/LinkButton.esm.js";
+import * as React from "react";
+import { space } from "gatsby-interface";
+export default function MyPage() {
+  return /*#__PURE__*/React.createElement(_Heading, null, /*#__PURE__*/React.createElement(_Avatar, null), /*#__PURE__*/React.createElement(_LinkButton, null));
+}
