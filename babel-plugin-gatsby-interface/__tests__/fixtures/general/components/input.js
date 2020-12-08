3d40d07b7be0028f2b2b8cf1574da41f73c49454
Ward Peeters <ward@coding-tech.com>
2020-12-08 11:17:05 +0100

    feat(babel-plugin-gatsby-interface): improve treeshaking in gatsby applications (#426)

---
+++ babel-plugin-gatsby-interface/__tests__/fixtures/general/components/input.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { LinkButton, Avatar, space } from "gatsby-interface"
+import { Heading } from "gatsby-interface"
+
+export default function MyPage() {
+  return (
+    <Heading>
+      <Avatar />
+      <LinkButton />
+    </Heading>
+  )
+}
