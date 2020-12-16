69ac023c56a8842ca32833322eb6875b057052d2
Greg Lobinski <greglobinski@gmail.com>
2020-12-16 10:02:45 +0100

    feat: add SUCCESS tone and withBorder prop

--- src/components/NumberBadge/NumberBadge.stories.tsx
+++ src/components/NumberBadge/NumberBadge.stories.tsx
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import * as React from "react"
 import { jsx } from "@emotion/core"
 import { Meta, Story } from "@storybook/react"
 import { withVariationsContainer } from "../../utils/storybook"
@@ -15,19 +16,36 @@ const Template: Story<NumberBadgeProps> = args => <NumberBadge {...args} />
 export const Basic = Template.bind({})
 
 Basic.args = {
-  children: 8,
+  children: 80,
 }
 
-const TONES: NumberBadgeTone[] = [`NEUTRAL`, `WARNING`, `DANGER`]
+const TONES: NumberBadgeTone[] = [`NEUTRAL`, `WARNING`, `DANGER`, `SUCCESS`]
 
 export const Tones = () =>
   TONES.map(tone => (
     <StoryPropVariant propName="tone" propValue={tone}>
-      <NumberBadge key={tone} tone={tone}>
-        8
+      <NumberBadge key={tone} tone={tone} withBorder={true}>
+        80
       </NumberBadge>
     </StoryPropVariant>
   ))
+
+Tones.story = {
+  decorators: [withVariationsContainer],
+}
+
+export const withBorder = () => (
+  <React.Fragment>
+    <StoryPropVariant propName="withBorder" propValue={false}>
+      <NumberBadge tone={`SUCCESS`}>80</NumberBadge>
+    </StoryPropVariant>
+    <StoryPropVariant propName="withBorder" propValue={true}>
+      <NumberBadge tone={`SUCCESS`} withBorder={true}>
+        80
+      </NumberBadge>
+    </StoryPropVariant>
+  </React.Fragment>
+)
 
 Tones.story = {
   decorators: [withVariationsContainer],
