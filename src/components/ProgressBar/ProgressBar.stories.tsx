079d27ac91bd576eadbcbd5a350591855c33c34c
Greg Lobinski <greglobinski@gmail.com>
2021-02-17 00:15:45 +0100

    feat: add second optional value to render in ProgressBar

--- src/components/ProgressBar/ProgressBar.stories.tsx
+++ src/components/ProgressBar/ProgressBar.stories.tsx
@@ -107,3 +107,21 @@ export const CustomHeight = () => {
 CustomHeight.story = {
   decorators: [withVariationsContainer],
 }
+
+export const WithSecondValue = () => {
+  return (
+    <React.Fragment>
+      <ProgressBar
+        value={30}
+        secondValue={50}
+        max={100}
+        aria-describedby="remaining-days-1"
+        height={10}
+      />
+    </React.Fragment>
+  )
+}
+
+WithSecondValue.story = {
+  decorators: [withVariationsContainer],
+}
